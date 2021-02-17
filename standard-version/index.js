3f7c5d19c967b8cfe700530e191362c890b7ed83
zpffork <2373721525@qq.com>
2021-02-17 11:54:25 +0800

    feat: 在standard-version中加入git usernam为tag的前辍

--- standard-version/index.js
+++ standard-version/index.js
@@ -1,18 +1,22 @@
 const { install, packageJson, file, json } = require("mrm-core");
+const execSync = require("child_process").execSync;
 const execCommand = require("mrm-core/src/util/execCommand");
 const PKG_NAME = "package.json";
 
-module.exports = function task({ devDependencies }) {
+module.exports = function task({ devDependencies, prefixTag }) {
 	json(".versionrc.json").save();
 	install(devDependencies);
 	if (!file(PKG_NAME).exists()) {
 		execCommand(undefined, "npm", ["init", "-y"]);
 	}
+
 	const pkg = packageJson();
 	if (!pkg.get("version")) {
 		pkg.set("version", "1.0.0").save();
 	}
-	pkg.setScript("sv-release", "standard-version -n -r patch").save();
+	pkg
+		.setScript("sv-release", `standard-version -t ${prefixTag}_v -n -r patch`)
+		.save();
 	pkg
 		.merge({
 			"standard-version": {
@@ -29,5 +33,13 @@ module.exports.parameters = {
 	devDependencies: {
 		type: "config",
 		default: ["standard-version"],
+	},
+	prefixTag: {
+		type: "config",
+		default: function () {
+			const gitUserName = execSync("git config user.name").toString().trim();
+			if (!!gitUserName) return gitUserName;
+			return "default";
+		},
 	},
 };
